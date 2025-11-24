7bbe3b41070f02c1d245eb501c3eb4e5e376a4e6
Ord00 <ivansimonov7@yandex.ru>
2025-11-24 19:55:50 +0300

    Finished unit auth tests

--- AuthService/src/test/java/OrganizerRegisterServiceUnitTest.java
+++ AuthService/src/test/java/OrganizerRegisterServiceUnitTest.java
@@ -1,17 +1,17 @@
 import event.rec.service.enums.ErrorMessage;
 import event.rec.service.exceptions.RegisterUserException;
-import event.rec.service.requests.AdminRegistrationRequest;
-import event.rec.service.service.AdminRegisterService;
+import event.rec.service.requests.OrganizerRegistrationRequest;
+import event.rec.service.service.OrganizerRegisterService;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.kafka.requestreply.RequestReplyFuture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
+import org.springframework.kafka.requestreply.RequestReplyFuture;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.Duration;
@@ -26,40 +26,42 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class AdminRegisterServiceUnitTest {
+public class OrganizerRegisterServiceUnitTest {
 
     @Mock
-    private ReplyingKafkaTemplate<String, AdminRegistrationRequest, Boolean> adminRegisterTemplate;
+    private ReplyingKafkaTemplate<String, OrganizerRegistrationRequest, Boolean> organizerRegisterTemplate;
 
     @InjectMocks
-    private AdminRegisterService adminRegisterService;
+    private OrganizerRegisterService organizerRegisterService;
 
-    private AdminRegistrationRequest request;
+    private OrganizerRegistrationRequest request;
     private ConsumerRecord<String, Boolean> consumerRecord;
 
     @BeforeEach
     void setUp() throws ExecutionException, InterruptedException {
 
-        String adminRegisterRequestTopic = "admin-register-request";
-        ReflectionTestUtils.setField(adminRegisterService,
-                "registerAdminRequestTopic",
-                adminRegisterRequestTopic);
-        String adminRegisterResponseTopic = "admin-register-response";
-        ReflectionTestUtils.setField(adminRegisterService,
-                "registerAdminReplyTopic",
-                adminRegisterResponseTopic);
+        String organizerRegisterRequestTopic = "organizer-register-request";
+        ReflectionTestUtils.setField(organizerRegisterService,
+                "registerOrganizerRequestTopic",
+                organizerRegisterRequestTopic);
+        String organizerRegisterResponseTopic = "organizer-register-response";
+        ReflectionTestUtils.setField(organizerRegisterService,
+                "registerOrganizerReplyTopic",
+                organizerRegisterResponseTopic);
 
-        request = new AdminRegistrationRequest();
-        request.setFullName("Test User");
+        request = new OrganizerRegistrationRequest();
+        request.setOrganizerName("Test User");
         request.setLogin("testUser");
         request.setPassword("testPassword");
-        request.setUserType("ADMIN");
+        request.setUserType("ORGANIZER");
 
         consumerRecord = mock(ConsumerRecord.class);
-        RequestReplyFuture<String, AdminRegistrationRequest, Boolean> future = mock(RequestReplyFuture.class);
+        RequestReplyFuture<String, OrganizerRegistrationRequest, Boolean> future = mock(RequestReplyFuture.class);
 
         doReturn(consumerRecord).when(future).get();
-        doReturn(future).when(adminRegisterTemplate).sendAndReceive(any(ProducerRecord.class), any(Duration.class));
+        doReturn(future).when(organizerRegisterTemplate).sendAndReceive(
+                any(ProducerRecord.class),
+                any(Duration.class));
     }
 
     @Test
@@ -69,7 +71,7 @@ public class AdminRegisterServiceUnitTest {
 
         RegisterUserException exception = assertThrows(
                 RegisterUserException.class,
-                () -> adminRegisterService.registerUser(request)
+                () -> organizerRegisterService.registerUser(request)
         );
 
         assertEquals(ErrorMessage.USER_EXISTS.getMessage(), exception.getMessage());
@@ -80,6 +82,6 @@ public class AdminRegisterServiceUnitTest {
 
         when(consumerRecord.value()).thenReturn(Boolean.TRUE);
 
-        assertDoesNotThrow(() -> adminRegisterService.registerUser(request));
+        assertDoesNotThrow(() -> organizerRegisterService.registerUser(request));
     }
 }
